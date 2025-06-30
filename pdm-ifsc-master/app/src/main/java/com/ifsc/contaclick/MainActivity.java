0b6f5a2d0b44e8404339049002994c87204a3866
Jaque <jaqueline.c2004@aluno.ifsc.edu.br>
2025-06-30 09:12:14 -0300

    gps no pc do ifsc nao funciona, mas e para dar certo

--- pdm-ifsc-master/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ pdm-ifsc-master/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,67 +1,84 @@
 package com.ifsc.contaclick;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-
-import android.content.pm.ResolveInfo;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
-    PackageManager pm;
+    LocationManager locationManager;
+    TextView tvLatitude, tvLongitude, tvStatus;
 
-    List<ApplicationInfo> applicationInfoList;
+    Button button;
 
 
-  //  pm.query
-
-    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        lv=findViewById(R.id.listView);
+        tvLatitude = findViewById(R.id.tvLatitude);
+        tvLongitude = findViewById(R.id.tvLongitude);
+        tvStatus = findViewById(R.id.tvStatus);
+        button = findViewById(R.id.button);
 
-        pm=getPackageManager();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        Intent intentFilter =new Intent(Intent.ACTION_MAIN);
-        intentFilter.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> lRInfo = pm.queryIntentActivities(intentFilter,0);
-        applicationInfoList=new ArrayList<>();
-
-        for (ResolveInfo r:lRInfo){
-
-            applicationInfoList.add( r.activityInfo.applicationInfo);
-        }
-
-
-
-     //   applicationInfoList= pm.getInstalledApplications(PackageManager.MATCH_ALL);
-
-       AppAdapter appAdapter = new AppAdapter(this,R.layout.app_item,applicationInfoList);
-       lv.setAdapter(appAdapter);
-       lv.setOnItemClickListener((adapter, view, position, id) ->{
-           ApplicationInfo applicationInfo= (ApplicationInfo) adapter.getItemAtPosition(position);
-           Intent i =pm.getLaunchIntentForPackage(applicationInfo.packageName);
-           if(i!= null){
-               startActivity(i);
-           }else{
-               Toast.makeText(getApplicationContext(), "App não lançavel", Toast.LENGTH_LONG);
-           }
+        button.setOnClickListener(v -> {
+            getLocalizacao();
         });
     }
+    @SuppressLint("MissingPermission")
+    public void getLocalizacao(){
+checaESolicitaPermicao();
+if (checaESolicitaPermicao()) {
+    tvLatitude.setText("Buscando localizacao");
+
+    Location l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    if(l!=null){
+        tvLatitude.setText(Double.toString(l.getLatitude()));
+        tvLongitude.setText(Double.toString(l.getLongitude()));
+   // }
+}else {
+        tvLatitude.setText("Sem localizacao");
+    }
+}else{
+        tvLatitude.setText("Permissao negada");
+    }
+
+    }
+    public boolean checaESolicitaPermicao(){
+        //verifica se tem e caso nao pede a permiscap
+        if (
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED
+            ||
+                        ActivityCompat.checkSelfPermission(this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION)
+                                !=PackageManager.PERMISSION_GRANTED
+        ){  //CODIGO IF
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.ACCESS_COARSE_LOCATION,
+                                  Manifest.permission.ACCESS_FINE_LOCATION
+                                 },
+                    PackageManager.PERMISSION_GRANTED
+                    );
+            return false;
+        }
+        else{
+            return true;
+        }
+            }
 
 
 
